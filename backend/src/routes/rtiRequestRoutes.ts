1e26d8c800ee391d0cd040f14d769b6a4269e631
bibek-bhusal03 <bibekbhusal455@gmail.com>
2025-09-21 01:53:15 +0545

    update permissions for apis

--- backend/src/routes/rtiRequestRoutes.ts
+++ backend/src/routes/rtiRequestRoutes.ts
@@ -8,14 +8,66 @@ import {
   deleteRTIRequest,
 } from "../controllers/rtiRequestController";
 import { upload } from "../middlewares/multer";
+import { validateAccessToken } from "@baijanstack/express-auth";
+import { checkPermissionsMiddleware } from "../middlewares/auth";
+import { Role } from "../models/User";
 
 const router = express.Router();
 
-router.post("/", upload.array("attachments"), createRTIRequest);
-router.get("/", getAllRTIRequests);
-router.get("/user/:userId", getUserRTIRequests);
-router.get("/:id", getRTIRequestById);
-router.put("/:id", updateRTIRequest);
-router.delete("/:id", deleteRTIRequest);
+router.post(
+  "/",
+  validateAccessToken,
+  checkPermissionsMiddleware({
+    apiPermissions: [Role.ADMIN, Role.USER],
+  }),
+  upload.array("attachments"),
+  createRTIRequest
+);
+router.get(
+  "/",
+
+  validateAccessToken,
+  checkPermissionsMiddleware({
+    apiPermissions: [Role.ADMIN, Role.USER],
+  }),
+  getAllRTIRequests
+);
+router.get(
+  "/user/:userId",
+
+  validateAccessToken,
+  checkPermissionsMiddleware({
+    apiPermissions: [Role.ADMIN, Role.USER],
+  }),
+  getUserRTIRequests
+);
+router.get(
+  "/:id",
+
+  validateAccessToken,
+  checkPermissionsMiddleware({
+    apiPermissions: [Role.ADMIN, Role.USER],
+  }),
+
+  getRTIRequestById
+);
+router.put(
+  "/:id",
+
+  validateAccessToken,
+  checkPermissionsMiddleware({
+    apiPermissions: [Role.ADMIN, Role.USER],
+  }),
+  updateRTIRequest
+);
+router.delete(
+  "/:id",
+
+  validateAccessToken,
+  checkPermissionsMiddleware({
+    apiPermissions: [Role.ADMIN, Role.USER],
+  }),
+  deleteRTIRequest
+);
 
 export default router;
